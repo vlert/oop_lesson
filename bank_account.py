5e0064fef0f63ec8766961157bd54601ef213d9a
Supakorn Wanuchsoontorn <supakorn.wan@ku.th>
2023-11-08 10:09:41 +0700

    oo bank account simulation

--- bank_account.py
+++ bank_account.py
@@ -1,70 +1,44 @@
-account_database = []
+class Account:
+    def __init__(self, account_number, account_type, account_name, initial_balance):
+        self.account_number = account_number
+        self.account_type = account_type
+        self.account_name = account_name
+        self.balance = initial_balance
 
-def create_account(num, type, name, init_balance):
-    index = search_account_db(num)
-    if index == -1:
-        account = {}
-        account["account_number"] = num
-        account["type"] = type
-        account["account_name"] = name
-        account["balance"] = init_balance
-        account_database.append(account)
-    else:
-        print("Account", num, "already exists")
-
-def delete_account(num):
-    index = search_account_db(num)
-    if index != -1:
-        print("Deleting account:", account_database[index]["account_number"])
-        del account_database[index]
-    else:
-        print(num, "invalid account number; nothing to be deleted.")
-
-def search_account_db(num):
-    for i in range(len(account_database)):
-        if account_database[i]["account_number"] == num:
-            return i
-    return -1
-
-def deposit(account_num, amount):
-    index = search_account_db(account_num)
-    if index != -1:
-        print("Depositing", amount, "to", account_database[index]["account_number"])
-        account_database[index]["balance"] += amount
-    else:
-        print(account_num, "invalid account number; no deposit action performed.")
-
-def withdraw(account_num, amount):
-    index = search_account_db(account_num)
-    if index != -1:
-        if account_database[index]["balance"] >= amount:
-            print("Withdrawing", amount, "from", account_database[index]["account_number"])
-            account_database[index]["balance"] -= amount
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Depositing {amount} to {self.account_number}")
         else:
-            print("withdrawal amount", amount, "exceeds the balance of", account_database[index]["balance"], "for", account_num, "account.")
-    else:
-        print(account_num, "invalid account number; no withdrawal action performed.")
-        
-def show_account(account_num):
-    index = search_account_db(account_num)
-    if index != -1:
-        print("Showing details for", account_database[index]["account_number"])
-        print(account_database[index])
-    else:
-        print(account_num, "invalid account number; nothing to be shown for.")
+            print("Invalid deposit amount.")
 
-create_account("0000", "saving", "David Patterson", 1000)
-create_account("0001", "checking", "John Hennessy", 2000)
-create_account("0003", "saving", "Mark Hill", 3000)
-create_account("0004", "saving", "David Wood", 4000)
-create_account("0004", "saving", "David Wood", 4000)
-print(account_database)
-show_account('0003')
-deposit('0003', 50)
-show_account('0003')
-withdraw('0003', 25)
-show_account('0003')
-delete_account('0003')
-show_account('0003')
-deposit('0003', 50)
-withdraw('0001', 6000)
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrawing {amount} from {self.account_number}")
+        else:
+            print(
+                f"Withdrawal amount {amount} exceeds the balance of {self.balance} for {self.account_number} account.")
+
+    def show_account_details(self):
+        print(f"Showing details for {self.account_number}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Account Type: {self.account_type}")
+        print(f"Account Name: {self.account_name}")
+        print(f"Balance: {self.balance}")
+
+
+account1 = Account("0000", "saving", "David Patterson", 1000)
+account2 = Account("0001", "checking", "John Hennessy", 2000)
+
+account1.show_account_details()
+account1.deposit(50)
+account1.show_account_details()
+account1.withdraw(25)
+account1.show_account_details()
+
+account2.show_account_details()
+account2.deposit(300)
+account2.show_account_details()
+account2.withdraw(5000)
+
